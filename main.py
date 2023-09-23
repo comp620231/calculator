21afd2449c277fbbf20adf7ba2e4a8cf85478429
unknown <comp620231@mail.ru>
2023-09-23 14:25:39 +0300

    new

--- main.py
+++ main.py
@@ -1,32 +1,61 @@
 from tkinter import *
 
-
+expression = ""
 class Example(Frame):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        num = StringVar()
-        num.set(0)
-        self.label = Entry(text=num, justify=RIGHT)
-        self.label.pack()
 
-        Button(text=1).place(x=0, y=50)
-        Button(text=2).place(x=75, y=50)
-        Button(text=3).place(x=150, y=50)
-        Button(text=4).place(x=230, y=50)
-        Button(text=5).place(x=310, y=50)
-        Button(text=6).place(x=0, y=75)
-        Button(text=7).place(x=75, y=75)
-        Button(text=8).place(x=150, y=75)
-        Button(text=9).place(x=230, y=75)
-        Button(text=0).place(x=310, y=75)
-        Button(text="/").place(x=0, y=100)
-        Button(text="*").place(x=75, y=100)
-        Button(text="-").place(x=150, y=100)
-        Button(text="+").place(x=230, y=100)
-        Button(text="=").place(x=310, y=100)
+        self.grid(row=0, column=0, columnspan=4, sticky="nsew")
+
+        self.input_field = Entry(self, font='Arial 15 bold', width=24, state="readonly")
+        self.input_field.pack(fill=BOTH)
+
+        buttons = (('7', '8', '9', '/', '4'),
+                   ('4', '5', '6', '*', '4'),
+                   ('1', '2', '3', '-', '4'),
+                   ('0', '.', '=', '+', '4')
+                   )
+
+        expression = ""
+
+        button = Button(text='C', command=lambda: self.bt_clear())
+        button.grid(row=1, column=3, sticky="nsew")
+
+        for row in range(4):
+            for col in range(4):
+                Button( width=2, height=3, text=buttons[row][col],
+                       command=lambda row=row, col=col: self.btn_click(buttons[row][col])).grid(row=row + 2, column=col,
+                                                                                           sticky="nsew", padx=1,
+                                                                                           pady=1)
+
+    def btn_click(self,item):
+        global expression
+        try:
+            self.input_field['state'] = "normal"
+            expression += item
+            self.input_field.insert(END, item)
+
+            if item == '=':
+                result = str(eval(expression[:-1]))
+                self.input_field.insert(END, result)
+                expression = ""
+
+            self.input_field['state'] = "readonly"
+        except ZeroDivisionError:
+            self.input_field.delete(0, END)
+            self.input_field.insert(0, 'Ошибка (деление на 0)')
+        except SyntaxError:
+            self.input_field.delete(0, END)
+            self.input_field.insert(0, 'Ошибка')
+
+    def bt_clear(self):
+        global expression
+        self.input_field['state'] = "normal"
+        self.input_field.delete(0, END)
+        self.input_field['state'] = "readonly"
 
 def main():
     root = Tk()
